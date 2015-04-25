dea920e9b4ca80c3400955b66af2ec0d49f2f635
addicted <addicted22sounds@gmail.com>
2015-04-25 15:38:20 +0300

    reservation time period is positive

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,9 @@
 class Reservation < ActiveRecord::Base
   belongs_to :table
   validates_presence_of :start_time, :end_time, :table
-  validate :time_period_positive?
+  validate :validate_time_period
 
-  def time_period_positive?
+  def validate_time_period
     errors.add(:end_time, :time_period_negative) if start_time > end_time
   end
 end
